3347cf930cb6867648fa7383b2880bad4593e62f
Riccardo Serino <r.serino9@gmail.com>
2023-03-29 09:49:03 +0200

    aa

---
+++ script.js
@@ -0,0 +1,17 @@
+var hamburger = document.querySelector(".hamburger");
+var wrapper = document.querySelector(".wrapper");
+ hamburger.addEventListener("click", () => {
+    document.querySelector(".wrapper").classList.toggle("hover_collapse");
+    document.querySelector(".content").classList.toggle("contentresize");
+})
+
+
+const clearInput = () => {
+    const input = document.getElementsByTagName("input")[0];
+    input.value = "";
+  }
+  
+  const clearBtn = document.getElementById("clear-btn");
+  clearBtn.addEventListener("click", clearInput);
+  
+  
